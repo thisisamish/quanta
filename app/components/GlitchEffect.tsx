e9836f0d29a8f0490e8b8fa1c31015e94bebd942
Amish Verma <amishhverma@gmail.com>
2023-10-11 15:00:15 +0530

    fix: glitch effect auto changes

--- app/components/GlitchEffect.tsx
+++ app/components/GlitchEffect.tsx
@@ -1,49 +1,58 @@
 'use client';
 
-import { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
-const words = ['Robotics', 'AI', 'Arduino', 'Figma', 'React.js'];
+let words = ['Robotics', 'AI', 'Arduino', 'Figma', 'React.js'];
 
 const GlitchEffect = () => {
-	const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(
-		undefined
-	);
+	const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const smallLetters = 'abcdefghijklmnopqrstuvwxyz';
 	const [word, setWord] = useState(words[0]);
 	const textRef = useRef<HTMLSpanElement>(null);
 
-	const handleMouseOver = () => {
-		let iteration = 0;
-		setWord((prev) => words[(words.indexOf(prev) + 1) % words.length]);
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setWord((prev) => words[(words.indexOf(prev) + 1) % words.length]);
+		}, 3000);
 
-		clearInterval(intervalId);
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, []);
 
-		const newIntervalId = setInterval(() => {
-			const el = textRef.current as HTMLSpanElement;
+	useEffect(() => {
+		if (textRef.current) {
+			const el = textRef.current;
 			const elDatasetValue = el.dataset.value as string;
-			el.innerText = el.innerText
-				.split('')
-				.map((letter, index) => {
-					if (index < iteration) {
-						return elDatasetValue[index];
-					}
+			let iteration = 0;
 
-					return letters[Math.floor(Math.random() * 26)];
-				})
-				.join('');
+			const newIntervalId = setInterval(() => {
+				el.innerText = el.innerText
+					.split('')
+					.map((letter, index) => {
+						if (index < iteration) {
+							return elDatasetValue[index];
+						}
+						if (index === 0) {
+							return capitalLetters[
+								Math.floor(Math.random() * 26)
+							];
+						}
+						return smallLetters[Math.floor(Math.random() * 26)];
+					})
+					.join('');
 
-			if (iteration >= elDatasetValue.length) {
-				clearInterval(newIntervalId);
-			}
+				if (iteration >= elDatasetValue.length) {
+					clearInterval(newIntervalId);
+				}
 
-			iteration += 1 / 3;
-		}, 30);
-
-		setIntervalId(newIntervalId);
-	};
+				iteration += 1 / 3;
+			}, 50);
+		}
+	}, [word]);
 
 	return (
-		<span ref={textRef} data-value={word} onMouseOver={handleMouseOver}>
+		<span ref={textRef} data-value={word}>
 			{word}
 		</span>
 	);
