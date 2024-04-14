1dc9004fae5ea8d6a4ff57e8be19a24bb86defdd
Amish Verma <amishhverma@gmail.com>
2024-04-14 19:35:20 +0530

    modified nextconfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'nextjs.org',
+				port: '',
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
